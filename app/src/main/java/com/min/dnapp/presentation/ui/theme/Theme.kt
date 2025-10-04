b0246de0ef9e8bf0ca4269f7d5bc62daf7f5d985
min486 <smh92333@gmail.com>
2025-10-04 21:43:38 +0900

    feat: 커스텀 typography 구성

--- app/src/main/java/com/min/dnapp/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/min/dnapp/presentation/ui/theme/Theme.kt
@@ -1,14 +1,12 @@
 package com.min.dnapp.presentation.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -35,23 +33,23 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun DngoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val appTypography = getAppTypography()
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    CompositionLocalProvider(
+        LocalAppTypography provides appTypography
+    ) {
+        MaterialTheme(
+//            colorScheme = colorScheme,
+            content = content
+        )
     }
+}
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+object MomentoTheme {
+    val typography: AppTypography
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalAppTypography.current
 }
