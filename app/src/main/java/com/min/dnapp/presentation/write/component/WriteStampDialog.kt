b274e3b063aaccec8b558b2a1b23e52cc7c1f45e
min486 <smh92333@gmail.com>
2025-10-12 15:20:03 +0900

    feat: WriteFinishScreen - 스탬프 지급 Dialog 구현

--- app/src/main/java/com/min/dnapp/presentation/write/component/WriteStampDialog.kt
+++ app/src/main/java/com/min/dnapp/presentation/write/component/WriteStampDialog.kt
@@ -1,13 +1,11 @@
-package com.min.dnapp.presentation.write
+package com.min.dnapp.presentation.write.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,88 +13,59 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.min.dnapp.R
-import com.min.dnapp.presentation.ui.theme.DngoTheme
 import com.min.dnapp.presentation.ui.theme.MomentoTheme
-import com.min.dnapp.presentation.write.component.WriteStampDialog
 
 @Composable
-fun WriteFinishScreen() {
-
-    var showStampDialog by remember { mutableStateOf(true) }
-
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MomentoTheme.colors.white
+fun WriteStampDialog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onDismiss
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 20.dp),
-            verticalArrangement = Arrangement.SpaceBetween
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            color = MomentoTheme.colors.white
         ) {
             Column(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.padding(horizontal = 20.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Spacer(Modifier.height(100.dp))
+                Spacer(Modifier.height(20.dp))
                 Image(
-                    painter = painterResource(R.drawable.write_finish),
+                    painter = painterResource(R.drawable.write_stamp),
                     contentDescription = null
                 )
-                Spacer(Modifier.height(40.dp))
+                Spacer(Modifier.height(20.dp))
                 Text(
-                    text = "여행 기록 작성이 완료되었어요.",
+                    text = "스탬프 1개가 지급되었어요!",
                     style = MomentoTheme.typography.title01,
                     color = MomentoTheme.colors.grayW20
                 )
-            }
-
-            // 버튼 영역
-            Column(
-                modifier = Modifier.fillMaxWidth()
-            ) {
+                Spacer(Modifier.height(20.dp))
                 Box(
                     modifier = Modifier
-                        .clickable {  }
+                        .clickable { onConfirm() }
                         .fillMaxWidth()
                         .background(color = MomentoTheme.colors.brownBase, shape = RoundedCornerShape(10.dp))
                         .padding(vertical = 16.dp),
                     contentAlignment = Alignment.Center
                 ) {
                     Text(
-                        text = "홈으로",
+                        text = "확인했어요",
                         style = MomentoTheme.typography.body01,
                         color = MomentoTheme.colors.white
                     )
                 }
-                Spacer(Modifier.height(16.dp))
+                Spacer(Modifier.height(20.dp))
             }
         }
-    }
-
-    // 스탬프 지급 모달창
-    if (showStampDialog) {
-        WriteStampDialog(
-            onDismiss = { showStampDialog = false },
-            onConfirm = { showStampDialog = false }
-        )
-    }
-}
-
-@Preview
-@Composable
-fun WriteFinishScreenPreview() {
-    DngoTheme {
-        WriteFinishScreen()
     }
 }
