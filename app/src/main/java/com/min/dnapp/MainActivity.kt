fef5bbf2c2f55bbca5fc98ae7adef035a64c19c6
min486 <smh92333@gmail.com>
2025-10-17 19:58:51 +0900

    feat: BottomBar 구성 및 navigation 연결

--- app/src/main/java/com/min/dnapp/MainActivity.kt
+++ app/src/main/java/com/min/dnapp/MainActivity.kt
@@ -3,8 +3,8 @@ package com.min.dnapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -13,14 +13,14 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.min.dnapp.presentation.AppStartViewModel
-import com.min.dnapp.presentation.bell.BellScreen
-import com.min.dnapp.presentation.find.FindDetailScreen
 import com.min.dnapp.presentation.find.FindScreen
-import com.min.dnapp.presentation.home.HomeScreen
 import com.min.dnapp.presentation.home.HomeScreen2
 import com.min.dnapp.presentation.login.LoginScreen
+import com.min.dnapp.presentation.mypage.MypageScreen
+import com.min.dnapp.presentation.ui.component.MomentoBottomNav
 import com.min.dnapp.presentation.ui.theme.DngoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -34,20 +34,7 @@ class MainActivity : ComponentActivity() {
 //        enableEdgeToEdge()
         setContent {
             DngoTheme {
-//                MomentoApp()
-//                LoginScreen2()
-//                OnboardingScreen()
-//                OnboardingScreen2()
-//                OnboardingScreen3()
-//                ProfileSetupScreen()
-//                MypageScreen()
-//                SettingScreen()
-//                RecordWriteScreen()
-//                WriteFinishScreen()
-//                FindScreen()
-//                FindDetailScreen()
-//                HomeScreen2()
-                BellScreen()
+                MomentoApp()
             }
         }
     }
@@ -61,20 +48,39 @@ fun MomentoApp(
     val startDestination = if (isLogin) "home" else "login"
 
     val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route ?: "home"
 
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
+    Scaffold(
+        bottomBar = {
+            MomentoBottomNav(
+                currentRoute = currentRoute,
+                onNavItemClick = { route ->
+                    // 현재 경로와 다를때만 navigate 호출
+                    if (navController.currentDestination?.route != route) {
+                        navController.navigate(route)
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
         NavHost(
+            modifier = Modifier.padding(paddingValues),
             navController = navController,
-            startDestination = startDestination
+//            startDestination = startDestination
+            startDestination = "home"
         ) {
             composable("login") {
                 LoginScreen(navController = navController)
             }
-
             composable("home") {
-                HomeScreen(navController = navController)
+                HomeScreen2()
+            }
+            composable("explore") {
+                FindScreen()
+            }
+            composable("my") {
+                MypageScreen()
             }
         }
     }
